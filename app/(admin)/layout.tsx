31ed881efc1e0cde50f5396c3ce683b3ca6791bd
Nathaniel Tampus <nate@mugnavo.com>
2023-11-19 14:42:50 +0800

    sidebar nga custom as fuuuuck

---
+++ app/(admin)/layout.tsx
@@ -0,0 +1,15 @@
+import type { Metadata } from "next";
+import Sidebar from "~/components/Sidebar";
+
+export const metadata: Metadata = {
+  title: "Admin - Saint Michael Parish Church",
+};
+
+export default function AdminLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex bg-gray-100 text-gray-800">
+      <Sidebar />
+      <main className="flex justify-center">{children}</main>
+    </div>
+  );
+}
---
+++ components/Sidebar.tsx
@@ -0,0 +1,120 @@
+"use client";
+
+import {
+  IconBrowserCheck,
+  IconCalendarMonth,
+  IconCertificate,
+  IconFolder,
+  IconLayoutDashboard,
+  IconLogout2,
+  IconSquareRoundedChevronLeftFilled,
+  IconSquareRoundedChevronRightFilled,
+} from "@tabler/icons-react";
+
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import { useState } from "react";
+
+interface NavItem {
+  title: string;
+  icon: JSX.Element;
+  href: string;
+}
+
+const navItems: NavItem[] = [
+  { title: "Dashboard", icon: <IconLayoutDashboard size={24} />, href: "/dashboard" },
+  { title: "Calendar of Events", icon: <IconCalendarMonth size={24} />, href: "/calendar" },
+  { title: "Mass Reservation", icon: <IconBrowserCheck size={24} />, href: "/massreservation" },
+  { title: "Certificates", icon: <IconCertificate size={24} />, href: "/certificates" },
+  { title: "Deceased Inventory", icon: <IconFolder size={24} />, href: "/deceasedinventory" },
+];
+
+export default function Sidebar() {
+  const [expanded, setExpanded] = useState(false);
+  const pathname = usePathname();
+
+  return (
+    <nav
+      className={
+        "relative min-h-screen bg-gray-50 pb-16 pt-2 text-gray-700 transition-all" +
+        (expanded ? " w-60" : " w-20")
+      }
+    >
+      <div
+        className="group absolute right-0 h-full cursor-pointer border-r-8 border-gray-50 transition-all hover:border-indigo-50"
+        onClick={() => setExpanded(!expanded)}
+      >
+        {expanded ? (
+          <IconSquareRoundedChevronLeftFilled
+            fill="currentColor"
+            className="absolute -left-2 top-6 z-10 text-indigo-500 transition-all hover:text-indigo-400 group-hover:scale-105"
+            size={36}
+          />
+        ) : (
+          <IconSquareRoundedChevronRightFilled
+            fill="currentColor"
+            className="absolute -left-2 top-6 z-10 text-indigo-500 transition-all hover:text-indigo-400 group-hover:scale-105"
+            size={36}
+          />
+        )}
+      </div>
+      <div className="absolute left-0 h-full border-l-8 border-gray-50"></div>
+      <div className="flex h-full w-full flex-col flex-wrap gap-8 overflow-hidden">
+        <div className="flex items-center">
+          <div className="inline-flex h-20 w-20 items-center justify-center">
+            <Image src="/logo.png" alt="Saint Michael Parish Church" width={42} height={42} />
+          </div>
+          <h1 className={"w-36 text-lg font-bold transition-all" + (expanded ? "" : " opacity-0")}>
+            Saint Michael Parish Church
+          </h1>
+        </div>
+        <div className="flex flex-1 flex-col justify-between">
+          <ul className="flex flex-col">
+            {navItems.map((item) => (
+              <li key={item.href}>
+                <Link
+                  className={
+                    "group flex items-center transition-colors" +
+                    (pathname.startsWith(item.href) ? " pointer-events-none" : "")
+                  }
+                  href={item.href}
+                >
+                  <div
+                    className={
+                      "absolute left-3 right-3 h-14 rounded-md transition-all group-hover:bg-indigo-100" +
+                      (expanded ? " w-54" : " w-14") +
+                      (pathname.startsWith(item.href) ? " bg-indigo-200" : "")
+                    }
+                  ></div>
+                  <div className="z-10 inline-flex h-16 w-20 items-center justify-center">
+                    {item.icon}
+                  </div>
+                  <span
+                    className={
+                      "z-10 -ml-4 w-40 font-bold transition-all" + (expanded ? "" : " opacity-0")
+                    }
+                  >
+                    {item.title}
+                  </span>
+                </Link>
+              </li>
+            ))}
+          </ul>
+          {/* TODO: logout */}
+          <a className="flex items-center">
+            <div className="inline-flex h-16 w-20 items-center justify-center">
+              <IconLogout2 size={24} />
+            </div>
+            <span
+              className={"-ml-4 w-40 font-bold transition-all" + (expanded ? "" : " opacity-0")}
+            >
+              Logout
+            </span>
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+}
