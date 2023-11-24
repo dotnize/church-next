6e7b802444c72b4e8b2054f2361a9050e1ccbe71
Nathaniel Tampus <nate@mugnavo.com>
2023-11-24 11:36:03 +0800

    initial temp data, move priests to db soon

--- lib/config.ts
+++ lib/config.ts
@@ -5,3 +5,13 @@
 // mass hours
 
 // colors?
+
+// maybe temporary and just store in database?
+export const massTypes = ["Thanksgiving Mass", "Barangay Mass", "School Mass", "Fiesta Mass"];
+
+export const priests = [
+    "Rev. Fr. Antonio G. Zamora Jr.",
+    "Rev. Fr. Calixto B. Apigo",
+    "Rev. Fr. Virgilio P. Paras",
+    "Rev. Fr. Arnel A. Autida",
+];
