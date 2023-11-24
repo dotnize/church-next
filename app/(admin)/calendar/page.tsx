870b70d2f29c6bcaf61f384b8c01fec48e6230df
Nathaniel Tampus <nate@mugnavo.com>
2023-11-24 11:17:58 +0800

    finish calendar layout

--- app/(admin)/calendar/page.tsx
+++ app/(admin)/calendar/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { IconMenu2 } from "@tabler/icons-react";
+import { IconChevronLeft, IconChevronRight, IconMenu2 } from "@tabler/icons-react";
 import {
+  add,
   compareAsc,
   eachDayOfInterval,
   endOfMonth,
@@ -10,6 +11,7 @@ import {
   getDay,
   isSameMonth,
   parse,
+  setMonth,
   startOfToday,
   startOfWeek,
 } from "date-fns";
@@ -18,6 +20,20 @@ import { useState } from "react";
 export default function Calendar() {
   const today = startOfToday();
   const days = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
   const colStartClasses = [
     "",
     "col-start-2",
@@ -28,7 +44,7 @@ export default function Calendar() {
     "col-start-7",
   ];
 
-  const [currMonth, setCurrMonth] = useState(() => format(today, "MMM-yyyy")); // TODO: select months in sidebar
+  const [currMonth, setCurrMonth] = useState(() => format(today, "MMM-yyyy"));
   let firstDayOfMonth = parse(currMonth, "MMM-yyyy", new Date());
 
   const [selectedDate, setSelectedDate] = useState(startOfToday);
@@ -39,23 +55,55 @@ export default function Calendar() {
     end: endOfWeek(endOfMonth(firstDayOfMonth)),
   });
 
+  const getPrevYear = () => {
+    const prevYearMonth = add(firstDayOfMonth, { years: -1 });
+    setCurrMonth(format(prevYearMonth, "MMM-yyyy"));
+  };
+  const getNextYear = () => {
+    const prevYearMonth = add(firstDayOfMonth, { years: 1 });
+    setCurrMonth(format(prevYearMonth, "MMM-yyyy"));
+  };
+
   return (
     <div className="flex justify-center p-8">
-      <div className="flex overflow-hidden rounded-lg border-2 border-black bg-white">
+      <div className="flex space-x-4 overflow-hidden rounded-lg border-2 border-black bg-white">
         <div
-          className={`relative h-full bg-indigo-400 transition-all ${showMonths ? "w-48" : "w-0"}`}
+          className={`relative h-full bg-indigo-400 text-indigo-50 transition-all ${
+            showMonths ? "w-44" : "w-0"
+          }`}
         >
           <button
             onClick={() => setShowMonths(!showMonths)}
-            className="absolute -right-9 z-10 rounded-br-lg bg-indigo-400 p-1.5 text-indigo-50"
+            className="absolute -right-10 z-10 rounded-br-lg bg-indigo-400 p-1.5"
           >
-            <IconMenu2 />
+            <IconMenu2 size={30} />
           </button>
-          <div className={`transition-all ${!showMonths && "-z-10 select-none opacity-0"}`}>
-            todo list months
+          <div
+            className={`flex h-full flex-col items-center justify-center gap-2 text-lg font-semibold transition-all ${
+              !showMonths && "-z-10 select-none opacity-0"
+            }`}
+          >
+            <div className="flex items-center gap-1 text-2xl">
+              <IconChevronLeft className="cursor-pointer" onClick={getPrevYear} size={32} />
+              {firstDayOfMonth.getFullYear()}
+              <IconChevronRight className="cursor-pointer" onClick={getNextYear} size={32} />
+            </div>
+            <div className="flex w-full flex-col">
+              {months.map((m, i) => (
+                <div
+                  className={`cursor-pointer p-1.5 px-8 hover:bg-indigo-300 ${
+                    firstDayOfMonth.getMonth() === i && "bg-indigo-500"
+                  }`}
+                  key={m}
+                  onClick={() => setCurrMonth(format(setMonth(firstDayOfMonth, i), "MMM-yyyy"))}
+                >
+                  {m}
+                </div>
+              ))}
+            </div>
           </div>
         </div>
-        <div className="ml-9 h-[564px] w-[600px] py-4">
+        <div className="h-[564px] w-[600px] py-4">
           <p className="flex justify-center text-2xl font-semibold uppercase text-indigo-500">
             {format(firstDayOfMonth, "MMMM yyyy")}
           </p>
