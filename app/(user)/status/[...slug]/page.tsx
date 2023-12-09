532595b8b462c77050f6113937c814be1afa7e1b
Nathaniel Tampus <nate@mugnavo.com>
2023-12-09 23:43:44 +0800

    linked status page for certificate requests

---
+++ app/(user)/status/[...slug]/page.tsx
@@ -0,0 +1,37 @@
+import { Image, Link } from "@nextui-org/react";
+import { redirect } from "next/navigation";
+
+import StatusPage from "~/components/StatusPage";
+
+export default function Status({ params }: { params: { slug: string[] } }) {
+  const [typeParam, idParam] = params.slug;
+  if (
+    !typeParam ||
+    !idParam ||
+    !["c", "m", "b", "d"].includes(typeParam) ||
+    isNaN(parseInt(idParam))
+  ) {
+    redirect("/");
+  }
+  const id = parseInt(idParam);
+  const type = typeParam as "c" | "m" | "b" | "d";
+
+  return (
+    <div className="flex flex-col items-center gap-6">
+      <div className="flex items-center gap-2 text-xl font-bold">
+        <Image src="/logo.png" alt="Saint Michael Parish Church" width={64} height={64} />
+        Saint Michael Parish Church
+      </div>
+      <div className="flex flex-col gap-2">
+        <Link
+          href="/"
+          color="foreground"
+          className="self-start font-semibold transition-all hover:underline"
+        >
+          &larr; Back to home
+        </Link>
+        <StatusPage type={type} id={id} />
+      </div>
+    </div>
+  );
+}
---
+++ components/StatusPage.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import { Spinner, Tooltip } from "@nextui-org/react";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+import { getBaptismById } from "~/actions/baptism";
+import { getConfirmationById } from "~/actions/confirmation";
+import { getDeceasedById } from "~/actions/deceased";
+import { getMarriageById } from "~/actions/marriage";
+
+export default function Status({ type, id }: { type: "c" | "m" | "b" | "d"; id: number }) {
+  const pathname = usePathname();
+  const certType =
+    type === "c" ? "Confirmation" : type === "m" ? "Marriage" : type === "b" ? "Baptism" : "Death";
+
+  const [cert, setCert] = useState<any>(null);
+  const [loading, setLoading] = useState(true);
+  const [copiedLink, setCopiedLink] = useState(false);
+  const currentUrl = window.location.origin + pathname;
+
+  async function fetchCert() {
+    setLoading(true);
+    let data = null;
+    if (type === "c") {
+      data = await getConfirmationById(id);
+    } else if (type === "m") {
+      data = await getMarriageById(id);
+    } else if (type === "b") {
+      data = await getBaptismById(id);
+    } else if (type === "d") {
+      data = await getDeceasedById(id);
+    }
+    if (data?.length && data[0]?.id) setCert(data[0]);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchCert();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  function copyLink() {
+    if ("clipboard" in navigator) {
+      navigator.clipboard.writeText(currentUrl);
+    } else {
+      document.execCommand("copy", true, currentUrl);
+    }
+    setCopiedLink(true);
+    setTimeout(() => {
+      setCopiedLink(false);
+    }, 5000);
+  }
+
+  return (
+    <div className="flex flex-col gap-8 rounded-xl bg-white bg-opacity-70 p-10">
+      {loading && !cert && <Spinner />}
+
+      {!loading && !cert && <div>No certificate request found.</div>}
+
+      {cert && (
+        <>
+          <div className="text-2xl font-bold">{certType} Certificate Request</div>
+          <div className="flex flex-col gap-1">
+            <div className="text-lg font-medium">
+              Status:{" "}
+              <span
+                className={`${cert.status !== "Pending" && "font-bold"} ${
+                  cert.status === "Released" && "text-emerald-600"
+                } ${cert.status === "Invalid" && "text-red-500"}`}
+              >
+                {cert.status}
+              </span>
+            </div>
+            <div>Requested by: {cert.requester_name.split(" ")[0]}</div>
+          </div>
+          {cert.status !== "Released" && (
+            <div className="flex flex-col gap-1">
+              <span className="text-sm">Save the link below to check the status anytime:</span>
+              <Tooltip placement="bottom" isOpen={copiedLink} content="Link copied to clipboard.">
+                <input
+                  type="text"
+                  value={window.location.origin + pathname}
+                  readOnly
+                  onClick={copyLink}
+                  className="rounded-md bg-white p-2 shadow"
+                  onFocus={(e) => e.target.select()}
+                />
+              </Tooltip>
+            </div>
+          )}
+        </>
+      )}
+    </div>
+  );
+}
