354f7be2d4fa6290d52f60ceef050f2f8c89bf61
Nathaniel Tampus <nate@mugnavo.com>
2023-12-09 13:01:13 +0800

    update marriage cert schema

--- actions/marriage.ts
+++ actions/marriage.ts
@@ -12,7 +12,7 @@ export async function createMarriage(formData: FormData) {
     try {
         const db = await getConnection();
         await db.query("INSERT INTO marriagecert SET ?", {
-            legal_status: formData.get("legal_status"),
+            husband_legal_status: formData.get("husband_legal_status"),
             husband_name: formData.get("husband_name"),
             husband_actual_address: formData.get("husband_actual_address"),
             husband_age: formData.get("husband_age"),
@@ -21,6 +21,8 @@ export async function createMarriage(formData: FormData) {
             husband_place_of_baptism: formData.get("husband_place_of_baptism"),
             husband_father: formData.get("husband_father"),
             husband_mother: formData.get("husband_mother"),
+            husband_witness: formData.get("husband_witness"),
+            wife_legal_status: formData.get("wife_legal_status"),
             wife_name: formData.get("wife_name"),
             wife_actual_address: formData.get("wife_actual_address"),
             wife_age: formData.get("wife_age"),
@@ -29,11 +31,12 @@ export async function createMarriage(formData: FormData) {
             wife_place_of_baptism: formData.get("wife_place_of_baptism"),
             wife_father: formData.get("wife_father"),
             wife_mother: formData.get("wife_mother"),
-            witness: formData.get("witness"),
+            wife_witness: formData.get("wife_witness"),
             date_of_marriage: formData.get("date_of_marriage"),
             position: formData.get("position"),
             book_number: formData.get("book_number"),
             page_number: formData.get("page_number"),
+            entry_number: formData.get("entry_number"),
             parish_priest: formData.get("parish_priest"),
             solemnization_date: formData.get("solemnization_date"),
             solemnization_place: formData.get("solemnization_place"),
@@ -49,7 +52,7 @@ export async function updateMarriage(formData: FormData) {
         const db = await getConnection();
         await db.query("UPDATE marriagecert SET ? WHERE id = ?", [
             {
-                legal_status: formData.get("legal_status"),
+                husband_legal_status: formData.get("husband_legal_status"),
                 husband_name: formData.get("husband_name"),
                 husband_actual_address: formData.get("husband_actual_address"),
                 husband_age: formData.get("husband_age"),
@@ -58,6 +61,8 @@ export async function updateMarriage(formData: FormData) {
                 husband_place_of_baptism: formData.get("husband_place_of_baptism"),
                 husband_father: formData.get("husband_father"),
                 husband_mother: formData.get("husband_mother"),
+                husband_witness: formData.get("husband_witness"),
+                wife_legal_status: formData.get("wife_legal_status"),
                 wife_name: formData.get("wife_name"),
                 wife_actual_address: formData.get("wife_actual_address"),
                 wife_age: formData.get("wife_age"),
@@ -66,11 +71,12 @@ export async function updateMarriage(formData: FormData) {
                 wife_place_of_baptism: formData.get("wife_place_of_baptism"),
                 wife_father: formData.get("wife_father"),
                 wife_mother: formData.get("wife_mother"),
-                witness: formData.get("witness"),
+                wife_witness: formData.get("wife_witness"),
                 date_of_marriage: formData.get("date_of_marriage"),
                 position: formData.get("position"),
                 book_number: formData.get("book_number"),
                 page_number: formData.get("page_number"),
+                entry_number: formData.get("entry_number"),
                 parish_priest: formData.get("parish_priest"),
                 solemnization_date: formData.get("solemnization_date"),
                 solemnization_place: formData.get("solemnization_place"),
