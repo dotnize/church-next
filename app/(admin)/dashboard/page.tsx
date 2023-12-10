77bda5e51553bff255557cf7d796b8ce7e83006f
Nathaniel Tampus <nate@mugnavo.com>
2023-12-10 15:13:17 +0800

    finish line chart

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { Card, CardBody } from "@nextui-org/react";
-import { compareAsc, isToday } from "date-fns";
+import { Card, CardBody, Spinner } from "@nextui-org/react";
+import { compareAsc, format, isSameDay, isToday } from "date-fns";
 import { useEffect, useState } from "react";
 import { CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from "recharts";
 import { getBaptisms } from "~/actions/baptism";
@@ -9,51 +9,7 @@ import { getConfirmations } from "~/actions/confirmation";
 import { getDeceased } from "~/actions/deceased";
 import { getMarriages } from "~/actions/marriage";
 import { getMassReservations } from "~/actions/massreservation";
-
-const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
+import { dateFormatter } from "~/lib/utils";
 
 export default function Dashboard() {
   const [baptism, setBaptism] = useState<any>([]);
@@ -61,38 +17,94 @@ export default function Dashboard() {
   const [marriage, setMarriage] = useState<any>([]);
   const [deceased, setDeceased] = useState<any>([]);
   const [reservations, setReservations] = useState<any>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [data, setData] = useState<any>([]);
 
   async function fetchBaptism() {
-    const data = await getBaptisms();
-    setBaptism(data);
+    const res = await getBaptisms();
+    setBaptism(res);
+    return true;
   }
 
   async function fetchConfirmation() {
-    const data = await getConfirmations();
-    setConfirmation(data);
+    const res = await getConfirmations();
+    setConfirmation(res);
+    return true;
   }
 
   async function fetchMarriage() {
-    const data = await getMarriages();
-    setMarriage(data);
+    const res = await getMarriages();
+    setMarriage(res);
+    return true;
   }
 
   async function fetchDeceased() {
-    const data = await getDeceased();
-    setDeceased(data);
+    const res = await getDeceased();
+    setDeceased(res);
+    return true;
   }
 
   async function fetchReservations() {
-    const data = await getMassReservations();
-    setReservations(data);
+    const res = await getMassReservations();
+    setReservations(res);
+    return true;
+  }
+
+  function getCertData() {
+    // 14 days ago, to today
+    const dates = [];
+    for (let i = -14; i < 1; i++) {
+      dates.push(new Date(new Date().setDate(new Date().getDate() + i)));
+    }
+
+    const dateData = dates.map((date) => {
+      const dateString = format(date, "yyyy-MM-dd");
+
+      const bCount = baptism.filter(
+        (baptism: any) =>
+          baptism.status === "Released" && isSameDay(new Date(baptism.date_of_issue), date)
+      ).length;
+
+      const cCount = confirmation.filter(
+        (confirmation: any) =>
+          confirmation.status === "Released" &&
+          isSameDay(new Date(confirmation.date_of_issue), date)
+      ).length;
+      const mCount = marriage.filter(
+        (marriage: any) =>
+          marriage.status === "Released" && isSameDay(new Date(marriage.date_of_issue), date)
+      ).length;
+      const dCount = deceased.filter(
+        (deceased: any) =>
+          deceased.status === "Released" && isSameDay(new Date(deceased.date_of_issue), date)
+      ).length;
+
+      const count = bCount + cCount + mCount + dCount;
+      return { name: dateString, count };
+    });
+    setData(dateData);
+  }
+
+  async function fetchAll() {
+    setLoading(true);
+    await Promise.allSettled([
+      fetchBaptism(),
+      fetchConfirmation(),
+      fetchMarriage(),
+      fetchDeceased(),
+      fetchReservations(),
+    ]);
+    setLoading(false);
   }
 
   useEffect(() => {
-    fetchBaptism();
-    fetchConfirmation();
-    fetchMarriage();
-    fetchDeceased();
-    fetchReservations();
+    getCertData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [baptism, confirmation, marriage, deceased]);
+
+  useEffect(() => {
+    fetchAll();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const pendingCerts =
@@ -118,58 +130,50 @@ export default function Dashboard() {
 
   return (
     <div className="flex h-full items-center justify-center p-8">
-      <div className="flex flex-col gap-8">
-        <div className="text-4xl font-bold">Dashboard</div>
-        <div className="flex gap-6">
-          <div className="rounded-2xl bg-white px-4 py-8 shadow-md">
-            <LineChart
-              width={800}
-              height={400}
-              data={data}
-              margin={{
-                top: 5,
-                right: 30,
-                left: 20,
-                bottom: 5,
-              }}
-            >
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="name" />
-              <YAxis />
-              <Tooltip />
-              <Legend />
-              <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-              <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-            </LineChart>
-          </div>
-          <div className="flex flex-col justify-center gap-6">
-            <Card className="bg-indigo-50">
-              <CardBody className="flex flex-col items-end p-4">
-                <h2>Pending certificates</h2>
-                <span className="text-3xl font-bold">{pendingCerts}</span>
-              </CardBody>
-            </Card>
-            <Card className="bg-emerald-50">
-              <CardBody className="flex flex-col items-end p-4">
-                <h2>Released certificates</h2>
-                <span className="text-3xl font-bold">{releasedCerts}</span>
-              </CardBody>
-            </Card>
-            <Card className="bg-orange-50">
-              <CardBody className="flex flex-col items-end p-4">
-                <h2>Mass reservations today</h2>
-                <span className="text-3xl font-bold">{reservationsToday}</span>
-              </CardBody>
-            </Card>
-            <Card className="bg-yellow-50">
-              <CardBody className="flex flex-col items-end p-4">
-                <h2>Upcoming reservations</h2>
-                <span className="text-3xl font-bold">{upcomingReservations}</span>
-              </CardBody>
-            </Card>
+      {loading && <Spinner />}
+      {!loading && (
+        <div className="flex flex-col gap-8">
+          <div className="text-4xl font-bold">Dashboard</div>
+          <div className="flex gap-6">
+            <div className="rounded-2xl bg-white p-10 shadow-md">
+              <LineChart width={800} height={400} data={data}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" tickFormatter={dateFormatter} />
+                <YAxis type="number" domain={[0, "dataMax + 3"]} />
+                <Tooltip />
+                <Legend />
+                <Line type="monotone" dataKey="count" stroke="#8884d8" activeDot={{ r: 8 }} />
+              </LineChart>
+            </div>
+            <div className="flex flex-col justify-center gap-6">
+              <Card className="bg-indigo-50">
+                <CardBody className="flex flex-col items-end p-4">
+                  <h2>Pending certificates</h2>
+                  <span className="text-3xl font-bold">{pendingCerts}</span>
+                </CardBody>
+              </Card>
+              <Card className="bg-emerald-50">
+                <CardBody className="flex flex-col items-end p-4">
+                  <h2>Released certificates</h2>
+                  <span className="text-3xl font-bold">{releasedCerts}</span>
+                </CardBody>
+              </Card>
+              <Card className="bg-orange-50">
+                <CardBody className="flex flex-col items-end p-4">
+                  <h2>Mass reservations today</h2>
+                  <span className="text-3xl font-bold">{reservationsToday}</span>
+                </CardBody>
+              </Card>
+              <Card className="bg-yellow-50">
+                <CardBody className="flex flex-col items-end p-4">
+                  <h2>Upcoming reservations</h2>
+                  <span className="text-3xl font-bold">{upcomingReservations}</span>
+                </CardBody>
+              </Card>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,12 @@
+import { format } from "date-fns";
+
 export function getOrdinal(n: number | string) {
     const num = parseInt(n.toString());
     const s = ["th", "st", "nd", "rd"];
     const v = num % 100;
     return num + (s[(v - 20) % 10] || s[v] || s[0]);
+}
+
+export function dateFormatter(date) {
+    return format(new Date(date), "MMMM do, yyyy");
 }
